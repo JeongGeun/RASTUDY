ca8f99a4cfebb57b6fc0ab48eb704ef4032436be
JeongGeun <geun5310@naver.com>
2020-09-28 23:04:45 +0900

    props and state 형태 공부

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Myname from'./MyName'
+import Counter from './Counter'
 class App extends Component {
   render() {
+    const name='조정근'
     return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <Myname name="조정근"/>
+        <Counter/>
+        {/**
+          1.삼항연산자로 if문 구현 가능
+          2.react상에는 style을 줄 때
+          const style={
+              backgroundColor : 'black',
+              padding :'16px',
+              color : 'white',
+              fontSize : '36px'
+          }
+          3.className='{css파일이름}'으로 css파일을 불러올 수 있다.
+        **/}
+        {name==='조정근'?<div>'hi'</div>:<div>bye</div>}
+      
+      </div>
   );
 }
 }
